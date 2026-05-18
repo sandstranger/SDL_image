906be951aedb82f0aef6e2f1a9a415b4bfe78dd3
Evgeniy Mineev <e.mineev@crazypandagames.com>
2026-05-18 10:59:14 +0400

    swtich SDL2 to SDL3

--- Android.mk
+++ Android.mk
@@ -93,7 +93,7 @@ LOCAL_SRC_FILES :=  \
     src/IMG_xv.c
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
-LOCAL_C_INCLUDES += $(SDL_IMAGE_LOCAL_PATH)/../../../../../sdl2/include
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../../../sdl2-compat/src/main/jni/sdl2-compat/include
 LOCAL_C_INCLUDES += $(SDL_IMAGE_LOCAL_PATH)/../../../../../libtiff/src/main/jni/libtiff/libtiff
 LOCAL_C_INCLUDES += $(SDL_IMAGE_LOCAL_PATH)/../../../../../libtiff/src/main/jni/include
 
@@ -157,10 +157,10 @@ endif
 LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/include
 
 ifeq ($(APP_OPTIM),debug)
-	LOCAL_LDLIBS += $(SDL_IMAGE_LOCAL_PATH)/../../../../../sdl2/android-project/app/build/intermediates/merged_native_libs/debug/mergeDebugNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libSDL2.so
+	LOCAL_LDLIBS += $(SDL_IMAGE_LOCAL_PATH)/../../../../../sdl2-compat/build/intermediates/merged_native_libs/debug/mergeDebugNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libSDL2.so
 	LOCAL_LDLIBS += $(SDL_IMAGE_LOCAL_PATH)/../../../../../libtiff/build/intermediates/merged_native_libs/debug/mergeDebugNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libtiff.so
 else
-	LOCAL_LDLIBS += $(SDL_IMAGE_LOCAL_PATH)/../../../../../sdl2/android-project/app/build/intermediates/merged_native_libs/release/mergeReleaseNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libSDL2.so
+	LOCAL_LDLIBS += $(SDL_IMAGE_LOCAL_PATH)/../../../../../sdl2-compat/build/intermediates/merged_native_libs/release/mergeReleaseNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libSDL2.so
 	LOCAL_LDLIBS += $(SDL_IMAGE_LOCAL_PATH)/../../../../../libtiff/build/intermediates/merged_native_libs/release/mergeReleaseNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libtiff.so
 endif
 
